6069a1d901ef2d08664cbad520507af114b680ae
Yessica Salgado Fernandez <yessisafe@gmail.com>
2022-05-27 21:09:55 -0500

    agregando el proyecto terminado, y todos sus archivos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-
 const seccionesPagina = fullpage('#fullpage', {
     // ──────────────────────────────────────────────────
 	//   :::::: Opciones Basicas
@@ -14,11 +13,30 @@ const seccionesPagina = fullpage('#fullpage', {
 	easingcss3: 'ease-out',
 	loopBottom: false,
 
-//   :::::: Barra de navegación
+	//   :::::: Barra de navegación
 	// ──────────────────────────────────────────────────
     navigation: true,
 	menu: '#menu',
 	anchors: ['inicio', 'fotos', 'footer'],
 	navigationTooltips: ['Inicio', 'Fotos', 'Footer'],
 	showActiveTooltip: false,
+
+	//   :::::: Secciones
+	// ──────────────────────────────────────────────────
+	sectionsColor : ['#000', '#c2c2c2', '#000'], // Color de fondo de cada seccion.
+	verticalCentered: true, // Se alineara de forma vertical los contenidos de cada seccion.
+
+	//   :::::: Slides
+	// ──────────────────────────────────────────────────
+	controlArrows: true, // Flechas del slide
+	slidesNavigation: false, // Indicadores del slide
+// ──────────────────────────────────────────────────
+
+//   :::::: Animaciones (Callbacks de FullPage.js)
+	// ──────────────────────────────────────────────────
+	afterLoad: function(origin, destination){
+		if(destination.anchor == 'footer'){
+			 document.querySelector('.footer').querySelector('h2').style.opacity = 1;
+		}
+   }
 });
